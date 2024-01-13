51f0125af3ca0014ff154fc72c3129e64291962a
NinoTraverso <njt98git@proton.me>
2024-01-13 22:20:30 +0000

    created contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { ContactUs } from "./ContactForm";
 
 function Contact() {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer>
+      <div>
+        <ContactUs />
+      </div>
+
       <p>Copyright &copy; {currentYear}</p>
     </footer>
   );
