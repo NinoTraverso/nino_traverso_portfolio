b4155f286fefa3680026cece139962eea139c702
NinoTraverso <njt98git@proton.me>
2024-01-14 00:25:02 +0000

    change profile image and fixed some responsiveness

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,18 +19,33 @@ function Contact() {
         CONTACT
       </h1>
       <div className="d-flex flex-column flex-xl-row align-items-center mt-5">
-        <div className="w-75 mx-5 mt-3">
+        <div className="w-75 mx-5 mt-3 mb-5">
           <ContactUs />
         </div>
         <div
           id="infoContainer"
-          className="w-75 d-flex flex-row justify-content-center align-items-center"
+          className="w-75 d-flex flex-column flex-xl-row justify-content-center align-items-center mt-5"
         >
           <div id="infoSvgContainer" className="align-self-center">
             <img src="/assets/iconInfo.svg" alt="emailIcon" />
           </div>
-          <h1 className="">ABOUT ME</h1>
-          <p></p>
+          <div
+            id="infoTextContainer"
+            className="d-flex flex-column justify-content-start align-items.-center"
+          >
+            <h1 className="">ABOUT ME</h1>
+            <h4 className="me-5">
+              Hey there! I'm a passionate full-stack developer who thrives on
+              continuous learning. The dynamic world of web development and
+              design fuels my excitement for creating innovative solutions and
+              tackling new challenges daily. I love bringing ideas to life and
+              enhancing my tech knowledge. Beyond coding, I'm also a passionate
+              Astronomer, reason why I chose this was one of my Capstone
+              projects. I like working on different projects and topics; not
+              only it aids my development skills but also allows me to learn new
+              things in different fields.
+            </h4>
+          </div>
         </div>
       </div>
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -21,17 +21,20 @@ function createProjectCard(project) {
 
 function Projects() {
   return (
-    <div>
+    <div className="mt-5">
       <h1 id="projects" className="sectionTitle d-none d-xl-block">
         PROJECTS
       </h1>
       <h1
         id="projects"
-        className="sectionTitleMedium d-none d-md-block d-xl-none"
+        className="sectionTitleMedium d-none d-md-block d-xl-none text-center"
       >
         PROJECTS
       </h1>
-      <h1 id="projects" className="sectionTitleSmall d-block d-md-none">
+      <h1
+        id="projects"
+        className="sectionTitleSmall d-block d-md-none text-center"
+      >
         PROJECTS
       </h1>
 
