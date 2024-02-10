bbae1fc5d04b9df14749a3bea31d0051777204bf
NinoTraverso <njt98git@proton.me>
2024-02-10 16:35:38 +0000

    adjusted project observasky image path

--- src/projects.js
+++ src/projects.js
@@ -2,7 +2,7 @@ const projects = [
   {
     id: 1,
     name: "ObservaSky",
-    img: "/assets/ObservaSkyHover.png",
+    img: "/assets/observaSkyHover.png",
     description:
       "A platform designed for for an observatory where stargazers can keep track of upcoiming astronomy evenets, book astronomy lectures, read articles and explore the astronomy glossary.Astronomers maintain all the contents of the website, including users.",
     projectLink: "https://github.com/NinoTraverso/ObservaSky",
